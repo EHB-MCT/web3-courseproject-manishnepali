5650885248a795b8aef3be45d2f19777903dd228
manishnepali <manish.nepali@student.ehb.be>
2021-09-28 13:43:26 +0200

    api fectch

--- vue/script.js
+++ vue/script.js
@@ -1,6 +1,40 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        content: 'hello world from vue'
+const app = Vue.createApp({
+template: '<h1> hello {{firstname}} </h1> <ul v-for="post in posts" v-bind:key="post.id"> <li>{{ post.title }}</li><p>{{ post.body }}</p></ul>',
+data(){
+
+    return{
+        firstname : "stan",
+        posts: [],
     }
-});
+},
+methods: {
+    async getData() {
+      try {
+        let response = await fetch("http://jsonplaceholder.typicode.com/posts");
+        this.posts = await response.json();;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+
+  created() {
+    this.getData();
+  },
+})
+
+app.mount('#app')
+
+
+
+
+
+
+// var app = new Vue({
+//     el: '#app',
+//     data: {
+//         content: 'hello world from vue',
+//         image: './assets/hello.jpeg',
+//         isClint: true
+//     }
+// });
