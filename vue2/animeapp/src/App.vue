6a70333fd87a4c935373ab7248475b478ebbd4d0
manishnepali <manish.nepali@student.ehb.be>
2021-09-29 17:18:03 +0200

    tryinng to ge api call

---
+++ vue2/animeapp/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+<header>
+  <nav>
+    <ul>
+      <li id="logoList"><img src="./assets/logo.png" id="logo">
+      animelist</li>
+    </ul>
+  </nav>
+</header>
+  <main>
+  <HomePage/>
+</main>
+</template>
+
+<script>
+
+import HomePage from './components/HomePage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomePage,
+  },
+};
+</script>
+
+<style>
+*{
+  list-style: none;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  text-align: center;
+  color: white;
+  margin-top:20%;
+}
+nav{
+  overflow: hidden;
+  background-color: #333;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+</style>
---
+++ vue2/animeapp/src/components/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <div class ="anime">
+      <img src="../assets/anime.jpeg" id="big">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'animeapp',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home{
+text-align: center;
+}
+.anime{
+height: 70%;
+}
+#big{
+  width: 30%;
+}
+</style>
