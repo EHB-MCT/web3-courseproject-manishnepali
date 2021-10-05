3685ff7ec00a8daca13421e5f0126bccfa040a20
manishnepali <manish.nepali@student.ehb.be>
2021-10-05 16:18:26 +0200

    json

--- react/quiz-app/src/components/homepage.js
+++ react/quiz-app/src/components/homepage.js
@@ -4,8 +4,9 @@ import React, {Component, useState} from 'react';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 function HomePage(){
     return(  <Router>
-    <div id="homepage">
-            <h1>QUIZ-app by manish</h1>
+    <div class="quiz_form">
+        <h1>welcome to quiz-app</h1>
+            
            
               
                 
