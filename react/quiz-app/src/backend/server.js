b76f125ad7caa052d5e3fa45280691a3a61e5494
manishnepali <manish.nepali@student.ehb.be>
2021-10-06 17:34:39 +0200

    express fsWrritefile

--- react/quiz-app/src/backend/server.js
+++ react/quiz-app/src/backend/server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express'); //Line 1
 const app = express(); 
 const port = process.env.PORT || 3000; //Line 3
@@ -6,6 +7,8 @@ const port = process.env.PORT || 3000; //Line 3
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
 // create a GET route
-app.get('/express_backend', (req, res) => { //Line 9
+app.get('/api', (req, res) => { //Line 9
+  
+  fs.writeFileSync("../quiz_vragen.json", "hello world" )
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
 }); //Line 11
