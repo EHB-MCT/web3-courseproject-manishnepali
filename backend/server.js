da26e5be5f6a277cadd5b1126084429c1f394185
manishnepali <manish.nepali@student.ehb.be>
2021-10-13 15:02:27 +0200

    heroku

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,106 @@ app.use(bodyParser.urlencoded({
 const wRouter = express.Router(); //class which helps us to create router handlers
 const port = process.env.PORT || 3000; //zodat het ofwel local host of een andere url kan gebruiken
 
-app.get('/', (req, res)=> {
+//routes, als de node app.js gestart wordt wordt rotues.html weergegeven
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'routes.html'));
+  });
+
+  //api 
+  
+  wRouter.route('/myworkouts') 
+  //de ontvangen gegevens van frontend worden geplaats in mongodb
+  .post((res, req)=>{
+    collection = db.collection("workout");
+    collection.insertOne(req.body).then(()=>{
+        res.status(200).json({
+            message: 'added'
+        }).catch((error)=>(
+            res.status(400).json({
+            error: error
+            })
+        ));
+    })
+  })
+  //de data van mongo wordt ontvangen
+  .get((req, res)=>{
+  collection = db.collection("workout");
+  collection.find({}).toArray((error, result) =>{
+    if(error){
+      return res.status(500).send(error);
+    }
+    res.json(result);
+  })
+   
+  })
+  //geselecteerde objectid wordt verwijderd van database
+  .delete((req,res)=>{
+    collection = db.collection("workout");
+    collection.deleteOne({
+        "_id": ObjectID(req.body._id) 
+    }).then(
+        () => {
+          res.status(200).json({
+            message: 'Deleted!'
+          });
+        }
+      ).catch(
+        (error) => {
+          res.status(400).json({
+            error: error
+          });
+        }
+      );
 })
+
+.patch((req,res)=>{
+  collection = db.collection("workout");
+  try {const updatepost = collection.updateById({
+      "_id": ObjectID(req.body._id),
+      $set: { name: req.body.name,
+      description: req.body.description}
+  });
+  res.json(updatepost)
+} catch (err){
+res.json({message: err})
+}
+})
+
+
+
+
+
+  //krijg data van mongodb
+app.use('/api', wRouter);
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+    client.connect(err =>{
+      if(err){
+      throw err;
+    }
+    db = client.db(DB_WERKSTUK);
+  });
+
+  });
+//post new gegevens inn api (had eerst perongeluk verwijderd)
+wRouter.route('/mynewworkouts')
+.post((req, res) => {
+    collection = db.collection("workout");
+    collection.insertOne(req.body).then(
+      () => {
+        res.status(200).json({
+          message: 'Added!'
+        });
+      }
+    ).catch(
+      (error) => {
+        res.status(400).json({
+          error: error
+        });
+      });
+  })
+
+ 
+
+
+
