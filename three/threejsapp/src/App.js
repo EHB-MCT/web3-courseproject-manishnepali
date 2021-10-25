e3dc9f539c9c4247b85fc79a99d362c877929167
manishnepali <manish.nepali@student.ehb.be>
2021-10-25 11:15:47 +0200

    import 3d objex in js file

--- three/threejsapp/src/App.js
+++ three/threejsapp/src/App.js
@@ -1,8 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { Canvas, useThree } from "@react-three/fiber";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as THREE from "three";
 import "./App.css";
+import { useGLTF } from '@react-three/drei';
+import { Suspense } from 'react';
+
+
+
 const CameraController = () => {
   const { camera, gl } = useThree();
   useEffect(
@@ -24,17 +29,30 @@ function App() {
          <CameraController />
          <ambientLight />
          <spotLight intensity={0.3} position={[3, 13, 50]} />
-         <mesh position={[3, 0, 3]}>
-            <boxGeometry attach="geometry" args={[9, 2, 1]} />
-            <meshPhongMaterial attach="material" color="hotpink" />
-         </mesh>
-         
-         <mesh position={[3, 5, 3]}>
-            <cylinderGeometry attach="geometry" args={[4, 3, 4]} />
-            <meshPhongMaterial attach="material" color="darkred" />
-         </mesh>
+        
+         <Suspense fallback={null}>
+        <Model />
+      </Suspense>
+        
       </Canvas>
   );
 }
+
+
+function Model({ ...props }) {
+  const group = useRef()
+  const { nodes, materials } = useGLTF('/Predator_s.glb')
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <group scale={[0.22, 0.22, 0.22]}>
+        <mesh geometry={nodes.mesh0.geometry} material={materials.cabeza} />
+        <mesh geometry={nodes.mesh0_dat_polySurface13_1.geometry} material={materials.cuerpo} />
+        <mesh geometry={nodes.mesh0_1.geometry} material={materials.casco} />
+      </group>
+    </group>
+  )
+}
+
+useGLTF.preload('/Predator_s.glb')
 
 export default App;
