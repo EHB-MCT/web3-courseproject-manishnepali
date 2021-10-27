2a8a15c3b09db10056770635c45a8c5b8db42115
manishnepali <manish.nepali@student.ehb.be>
2021-10-27 16:01:22 +0200

    dagon balls objects

--- three/threejsapp/src/App.js
+++ three/threejsapp/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useSpring } from "react";
 import { Canvas, useThree, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as THREE from "three";
@@ -16,16 +16,16 @@ const CameraController = () => {
 
 
 function App() {
+  
   return (
     <Canvas>
          <CameraController />
          <ambientLight />
-         <spotLight intensity={0.3} position={[3, 13, 50]} />
+         <spotLight intensity={0.2} position={[9, 20, 80]} />
         
          <Suspense fallback={null}>
-        <Model />
-        <Model position={[0, 0, 0]} />
-        <Model position={[10, 0, -10]} />
+        <Model2 camera={{ fov: 75, position: [5, 0, 7] }} />
+        
       </Suspense>
         
       </Canvas>
@@ -34,12 +34,20 @@ function App() {
 
 
 function Model({ ...props }) {
+
+
   const group = useRef()
   const { nodes, materials } = useGLTF('/Predator_s.glb')
+  useFrame(() => {
+ 
+      group.current.rotation.y += 0.025;
+    
+  });
   return (
     <group ref={group} {...props} dispose={null}>
       <group scale={[0.22, 0.22, 0.22]}>
-        <mesh geometry={nodes.mesh0.geometry} material={materials.cabeza} />
+        
+        <mesh geometry={nodes.mesh0.geometry} material={materials.cabeza}  />
         <mesh geometry={nodes.mesh0_dat_polySurface13_1.geometry} material={materials.cuerpo} />
         <mesh geometry={nodes.mesh0_1.geometry} material={materials.casco} />
       </group>
@@ -48,5 +56,34 @@ function Model({ ...props }) {
 }
 
 useGLTF.preload('/Predator_s.glb')
+
+function Model2({ ...props }) {
+  const group = useRef()
+  const { nodes, materials } = useGLTF('/scene.gltf')
+  useFrame(() => {
+ 
+    group.current.rotation.z += -0.0025;
+  
+});
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <group rotation={[-Math.PI / 2, 0, 0]}>
+        <group rotation={[Math.PI / 2, 0, 0]}>
+          <group position={[-0.65, 0.32, -18.9]} rotation={[-Math.PI / 3.9, 0, 0]} scale={0.08}>
+            <mesh geometry={nodes['Dragon_Balls_07_-_Default_0'].geometry} material={materials['07_-_Default']} />
+            <mesh geometry={nodes['Dragon_Balls_09_-_Default_0'].geometry} material={materials['09_-_Default']} />
+            <mesh geometry={nodes['Dragon_Balls_08_-_Default_0'].geometry} material={materials['08_-_Default']} />
+            <mesh geometry={nodes['Dragon_Balls_13_-_Default_0'].geometry} material={materials['13_-_Default']} />
+            <mesh geometry={nodes['Dragon_Balls_01_-_Default_0'].geometry} material={materials['01_-_Default']} />
+            <mesh geometry={nodes['Dragon_Balls_02_-_Default_0'].geometry} material={materials['02_-_Default']} />
+            <mesh geometry={nodes['Dragon_Balls_03_-_Default_0'].geometry} material={materials['03_-_Default']} />
+          </group>
+        </group>
+      </group>
+    </group>
+  )
+}
+
+useGLTF.preload('/scene.gltf')
 
 export default App;
