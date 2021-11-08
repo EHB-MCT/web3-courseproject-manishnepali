e186761e37930f4bf05741f1c2cfe872158538b8
manishnepali <manish.nepali@student.ehb.be>
2021-11-08 11:12:49 +0100

    animejs

---
+++ animejs/script.js
@@ -0,0 +1,12 @@
+import anime from 'animejs/lib/anime.es.js';
+
+anime({
+    targets: 'div',
+    translateX: 100,
+    borderRadius: 50,
+    duration: 2000,
+    easing: 'linear',
+    direction: 'alternate'
+  });    
+  
+     
