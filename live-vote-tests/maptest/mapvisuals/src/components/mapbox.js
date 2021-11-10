01fa9218ea749718a2eb66aa283ac675af33da2d
manishnepali <manish.nepali@student.ehb.be>
2021-11-10 15:32:09 +0100

    pin ponnts by coordinates

--- live-vote-tests/maptest/mapvisuals/src/components/mapbox.js
+++ live-vote-tests/maptest/mapvisuals/src/components/mapbox.js
@@ -1,6 +1,9 @@
 import * as React from 'react';
+import '../App.css'
 import { useState } from 'react';
-import ReactMapGL,  {FlyToInterpolator} from 'react-map-gl';
+import Pin from '../assets/pin.svg'
+import ReactMapGL,  {FlyToInterpolator, Marker} from 'react-map-gl';
+import Votes from '../data/votes_cor.json'
 
 
 
@@ -8,11 +11,18 @@ export default function Map() {
   const [viewport, setViewport] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
-    latitude: 37.7577,
-    longitude: -122.4376,
+    longitude: 4.351697,
+    latitude: 50.8465573,
     zoom: 8
   });
 
+const bounds = [
+    [-74.04728, 40.68392], // [west, south]
+    [-73.91058, 40.87764]  // [east, north]
+];
+
+
+    
 
   const goToBrussel = () => {
     setViewport({
@@ -29,11 +39,38 @@ export default function Map() {
       <div>
     <button onClick={goToBrussel}>Brussel</button>
     <ReactMapGL
-    mapStyle="mapbox://styles/manishnepali/ckvtfhl8t1yrd14qnrcnwfryn"
+    
+    mapStyle="mapbox://styles/manishnepali/ckvtkgrx423me14t72cizk5n9"
     mapboxApiAccessToken={"pk.eyJ1IjoibWFuaXNobmVwYWxpIiwiYSI6ImNrdWNubDZ2cjEyNzEydW1va3VhMXIycDgifQ.n2XtlNGQ4lDXAJt65-Db3w"}
       {...viewport}
       onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
+    >
+        {Votes.map(vote =>(
+            <Marker key={vote.id}
+            latitude={vote.latitude}
+            longitude={vote.longitude}>
+                <button class="marker_btn">
+                <img src={Pin} />
+            </button>
+            </Marker>
+        ))}
+        <Marker latitude={50.8465573} longitude={4.351697}>
+            <button class="marker_btn">
+                <img src={Pin} />
+            </button>
+        </Marker>
+        </ReactMapGL>
+
     </div>
   );
 }
+// {data.map(vote => (
+//     <Marker
+//     key={vote.id}
+//     latitude={data.coordinates[1]}
+//     longitude={data.coordinates[0]}>
+//         <div>
+//             vote
+//         </div>
+//     </Marker>
+// ))}
