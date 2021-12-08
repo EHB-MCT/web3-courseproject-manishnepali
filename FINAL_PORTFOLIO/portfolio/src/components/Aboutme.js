2db7ea7def39973380709d1eea1fb827aa027c97
manishnepali <manish.nepali@student.ehb.be>
2021-12-08 16:39:07 +0100

    scroll magic & scroll trigger

--- FINAL_PORTFOLIO/portfolio/src/components/Aboutme.js
+++ FINAL_PORTFOLIO/portfolio/src/components/Aboutme.js
@@ -1,21 +1,43 @@
 import '../App.css';
 import { Controller, Scene } from 'react-scrollmagic';
 import Background from '../assets/background.jpg'
-import {useLayoutEffect, useState} from 'react'
+import {useEffect, useState} from 'react'
+import {gsap} from 'gsap'
 
 function Aboutme() {
+
+
+  useEffect(() => {
+    gsap.to("#test", {
+    x: 100,
+    duration: 5,
+    scrollTrigger: {
+      trigger: "#test",
+      markers: true,
+      start: "top center",
+      end: "bottom 80px",
+      scrub: true
+    }
+    });
+  }, []);
 
   return (
     <div className="aboutme">
     <div className="OnePager"
      style={{backgroundImage: `url(${Background})`}}>
     <Controller>
-      <Scene destroy={600} triggerElement="#naam" pin>
-        <div>hi</div>
-       
+      <Scene duration={500} pin>
+        <div id="aboutme_info">
+          <h1>About me</h1>
+        <p id="test" >Im a web developer, designer based in Brussels, Belguim. <br/>
+           Browse through this website to get to kown more about why i have chosen to become a web developer. <br/>
+           Discover projects i have made during my studies at multimedia & comunication technomlogy in Erasmus Hogeschool Buussel.
+         </p>
+         </div>
+
       </Scene>
     </Controller>
-   
+  
     </div>
     </div>
   );
