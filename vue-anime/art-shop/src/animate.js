2664b1841d3f1d0dc8d12d35ac9b5fb4d028153a
manishnepali <manish.nepali@student.ehb.be>
2021-12-14 18:50:01 +0100

    artshop

--- vue-anime/art-shop/src/animate.js
+++ vue-anime/art-shop/src/animate.js
@@ -6,3 +6,16 @@ export function translate(element) {
     translateX: 500
   });
 }
+
+export function resize(element) {
+  anime({
+    targets: element,
+    scale: 1.5,
+    translateX: 300,
+    backgroundColor: "hsl(200, 50%, 50%)",
+    easing: 'easeInOutQuad',
+    translate: 0
+   
+  });
+}
+
--- vue-anime/art-shop/src/components/Artlist.vue
+++ vue-anime/art-shop/src/components/Artlist.vue
@@ -1,20 +1,31 @@
 <template>
-<div id="home">
-    
-      <h3>{{artCollection.titles[0].title}}</h3>
-    <p>{{artCollection.artist}}</p>
+<div id="home" >
+    <li v-for="(art, index) in artCollection" :key="`art-${index}`" id="list">
+        <h3>{{art.titles[0].title}}</h3>
+       <p> {{art.artist[0]}} </p>
+
+
+        <img :src="art.image_thumbnail"
+        v-on:click="size"
+       v-on: 
+        ref="targeted"
+         id="imgg">
+     
+    </li>
+      <!-- <h3>{{artCollection.titles[0].title}}</h3> -->
+    <!-- <p>{{artCollection.artist}}</p>
     
    <span id="content">
     <img :src="artCollection.image_thumbnail" >
       <p>{{artCollection.labels[0].text}} <br/>{{artCollection.labels[0].source}}</p>
-  </span>
+  </span> -->
    
     
 
-  <!-- 
-   <p>{{artCollection.labels[0].text}} <br/>{{artCollection.labels[0].source}}</p>
-         <h3>{{artCollection.titles[0].title}}</h3>
-  -->
+  
+   <!-- <p>{{artCollection.labels[0].text}} <br/>{{artCollection.labels[0].source}}</p> -->
+         <!-- <h3>{{artCollection.titles[0].title}}</h3> -->
+ 
 
 </div>
 
@@ -22,6 +33,7 @@
 
 <script>
 import axios from 'axios'
+import {resize} from '../animate'
 
 export default {
   name: 'Artlist',
@@ -29,15 +41,16 @@ export default {
   },
   data() {
     return{
+     
       artCollection : []
     }
   },
   created(){
       axios
-        .get('https://api.smk.dk/api/v1/art/?object_number=kms1&lang=en')
+        .get('https://api.smk.dk/api/v1/art/search/?keys=all&offset=0&rows=10&lang=en')
             .then(response =>{
-                this.artCollection = response.data.items[0];
-                console.log(response.data.items[0]);
+                this.artCollection = response.data.items;
+                console.log(response.data);
                 console.log(response.data.items[0].titles[0].title);
 
                 console.log(response.data.items[0].image_thumbnail)
@@ -46,21 +59,40 @@ export default {
                 console.log(err)
             })
   },
+  methods: {
+        size() {
+          
+          console.log(event.path[0]);
+          console.log(this);
 
-  
+              resize(event.path[0]);
+          
+        }
+  },
+// https://api.smk.dk/api/v1/art/search/?keys=*&offset=0&rows=10&lang=en
+//  'https://api.smk.dk/api/v1/art/?object_number=kms1&lang=en' 
 }
 </script>
 
 <style>
 #home{
-  
+  height: 70%;
   font-family: garamond-premier-pro, serif;
-  padding: 5%;
   display: flex;
+  flex-flow: column;
+  list-style: none;
+ 
+
+}
+#list{
+  height: 30%;
+  padding: 5%;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-   gap: 1%;
+   align-content: center;
+   
+
+
+
 }
  h3{
     font-family: garamond-premier-pro, serif;
@@ -69,15 +101,10 @@ export default {
     font-size: 2em;
     color: black;
   }
-  #content{
-    display: flex;
-  flex-direction: row;
-  gap: 5%;
-  }
-  #content img{
-    width:200%;
-    height: 200%;
-    
+ 
+  #imgg{
+
+    width: 40%;
       
   }
 
